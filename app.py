9608bb4be6f8b91df520fc6b40a7dc3a9227eb7f
krxhna <m.krishnakanth1999@gmail.com>
2022-07-14 07:59:06 +0530

    new fiannce oath

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import yfinance as yf
 
 
 # configuration
-DEBUG = True
+
 
 # instantiate the app
 app = Flask(__name__)
@@ -14,12 +14,61 @@ app.config.from_object(__name__)
 CORS(app, resources={r'/*': {'origins': '*'}})
 
 
-# sanity check route
+
 @app.route('/info/<ticker>', methods=['GET'])
 def info(ticker):
     msft = yf.Ticker(ticker)
     return jsonify(msft.info)
 
 
+@app.route('/finance/<ticker>', methods=['GET'])
+def get_finance(ticker):
+    object = yf.Ticker(ticker)
+    #gwttng the finance thing
+    finance = object.financials
+    #transposing
+    finance_t = finance.transpose()
+    #repalce nuil
+    finance_t = finance_t.fillna(0).rename(columns=str.lower)
+    #remove space
+    finance_t.columns = finance_t.columns.str.replace(' ','_')
+
+
+    #picking up the first row 
+    res = finance_t.iloc[0].to_dict()
+    return jsonify(res)
+
+    
+
+
+
+@app.route('/hello', methods=['GET'])
+def hello():
+    return jsonify("hola")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
