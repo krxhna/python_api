efae9ce29ac369ff114304ec0349122dfcd3b00b
krxhna <m.krishnakanth1999@gmail.com>
2022-07-17 01:12:04 +0530

    iloc fixed index

--- app.py
+++ app.py
@@ -77,7 +77,18 @@ def get_balancesheet(ticker):
 
     #picking up the first row 
     res = finance_t.iloc[0].to_dict()
-    return jsonify(
+    
+    if len(finance_t) > 2:
+        return jsonify(
+                {
+            'symbol': ticker,
+            '2021': finance_t.iloc[0].to_dict(),
+            # '2020': finance_t.iloc[1].to_dict(),
+            # '2019': finance_t.iloc[2].to_dict(),
+            # '2018': finance_t.iloc[3].to_dict()
+        })
+    else:
+        return jsonify(
                 {
             'symbol': ticker,
             '2021': finance_t.iloc[0].to_dict(),
